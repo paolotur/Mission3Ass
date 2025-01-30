eec668f8a1a57378a241ca26af4c1f73c12da5ab
paolotur <paolotur@byu.edu>
2025-01-29 22:11:56 -0700

    More life

---
+++ FoodItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FoodItem
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Quantity { get; set; }
+    public DateTime ExpirationDate { get; set; }
+
+    public FoodItem(string name, string category, int quantity, DateTime expirationDate)
+    {
+        Name = name;
+        Category = category;
+        Quantity = quantity > 0 ? quantity : throw new ArgumentException("Quantity must be positive.");
+        ExpirationDate = expirationDate;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Category}) - {Quantity} units - Expiration Date: {ExpirationDate:MM/dd/yyyy}";
+    }
+}
